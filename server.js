ffba035c40fa50ef61c4c1cff898ac4af9bd3c49
lleifhei <loganleifheit@gmail.com>
2025-04-22 23:45:20 -0400

    built and connected to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { Client } = require('pg')
 const app = express()
 const port = 3000
 
@@ -6,6 +7,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, async() => {
+    // Connect to the database
+    await Client.connect({
+        user: 'user',
+        host: 'localhost',
+        database: 'postgres',
+        password: '',
+        port: 5432,
+    })
+    console.log('Connected to the database')
   console.log(`Example app listening on port ${port}`)
 })
