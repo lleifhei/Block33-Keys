a1742669b975b7ef7f4c4619d64bfa7e231335f1
lleifhei <loganleifheit@gmail.com>
2025-04-24 16:51:23 -0400

    finished routes

--- server.js
+++ server.js
@@ -12,7 +12,68 @@ const app = express()
 app.use(express.json())
 const port = 3000
 
+<<<<<<< HEAD
 
+=======
+app.get('/api/employees', (req, res) => {
+    client.query('SELECT * FROM employees', (err, result) => {
+        if (err) {
+            console.error(err)
+            res.status(500).send('Error executing query')
+        } else {
+            res.json(result.rows)
+        }
+    })
+})
+>>>>>>> ab16ec9 (finished routes)
+
+app.get('/api/departments', (req, res) => {
+    client.query('SELECT * FROM departments', (err, result) => {
+        if (err) {
+            console.error(err)
+            res.status(500).send('Error executing query')
+        } else {
+            res.json(result.rows)
+        }
+    })
+})
+
+app.post('/api/employees', (req, res) => {
+    const { name, department_id } = req.body
+    client.query('INSERT INTO employees (name, department_id) VALUES ($1, $2)', [name, department_id], (err, result) => {
+        if (err) {
+            console.error(err)
+            res.status(500).send('Error executing query')
+        } else {
+            res.status(201).send('Employee added')
+        }
+    })
+}
+)
+app.delete('/api/employees/:id', (req, res) => {
+    const id = req.params.id
+    client.query('DELETE FROM employees WHERE id = $1', [id], (err, result) => {
+        if (err) {
+            console.error(err)
+            res.status(500).send('Error executing query')
+        } else {
+            res.status(200).send('Employee deleted')
+        }
+    })
+})
+app.put('/api/employees/:id', (req, res) => {
+    const id = req.params.id
+    const { name, department_id } = req.body
+    client.query('UPDATE employees SET name = $1, department_id = $2 WHERE id = $3', [name, department_id, id], (err, result) => {
+        if (err) {
+            console.error(err)
+            res.status(500).send('Error executing query')
+        } else {
+            res.status(200).send('Employee updated')
+        }
+    })
+})
+  
 
 app.listen(port, async() => {
     // Connect to the database
